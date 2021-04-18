0aa1c7b22f960b368fbdfa5b1564a9b959294a87
ruffsl <roxfoxpox@gmail.com>
2021-04-18 10:31:02 -0700

    Stage subverb template from colcon metadata

--- colcon_snapshot/verb/snapshot.py
+++ colcon_snapshot/verb/snapshot.py
@@ -1,32 +1,32 @@
-# Copyright 2021 Ruffin White
+# Copyright 2016-2018 Dirk Thomas
 # Licensed under the Apache License, Version 2.0
 
-from colcon_core.package_selection import add_arguments \
-    as add_packages_arguments
-from colcon_core.package_selection import get_package_descriptors
+from colcon_core.command import add_subparsers
 from colcon_core.plugin_system import satisfies_version
 from colcon_core.verb import VerbExtensionPoint
+from colcon_metadata.subverb import get_subverb_extensions
 
 
-class SnapshotVerb(VerbExtensionPoint):
-    """Snapshot packages."""
+class MetadataVerb(VerbExtensionPoint):
+    """Manage metadata of packages."""
 
     def __init__(self):  # noqa: D107
         super().__init__()
         satisfies_version(VerbExtensionPoint.EXTENSION_POINT_VERSION, '^1.0')
+        self._subparser = None
 
     def add_arguments(self, *, parser):  # noqa: D102
-        # only added so that package selection arguments can be used
-        # which use the build directory to store state information
-        parser.add_argument(
-            '--build-base',
-            default='build',
-            help='The base path for all build directories (default: build)')
+        # remember the subparser to print usage in case no subverb is passed
+        self._subparser = parser
 
-        add_packages_arguments(parser)
+        # get subverb extensions and let them add their arguments
+        subverb_extensions = get_subverb_extensions()
+        add_subparsers(
+            parser, 'colcon metadata', subverb_extensions,
+            attribute='subverb_name')
 
     def main(self, *, context):  # noqa: D102
-        args = context.args
-
-        # descriptors = get_package_descriptors(args)
-        _ = get_package_descriptors(args)
+        # error: no subverb provided
+        if context.args.subverb_name is None:
+            print(self._subparser.format_usage())
+            return 'Error: No subverb provided'
